ed8efec1b07775df3a05e7be9dc1e7e27c8554ca
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-09-06 16:26:28 -0700

    Warm Up 2

---
+++ warmup2/main.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+var drink = ['water', 'coffee']
+
+function flip(drink) {
+  var flipdrink = ['coffee', 'water']
+  for (var i = 0; i > drink.length; i--) {
+    flipdrink = drink
+  }
+  return flipdrink
+}
