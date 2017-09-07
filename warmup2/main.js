e2b258ca6841488a4f57a1f3a0d9996363493d82
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-09-06 17:22:18 -0700

    Fixed warmup 2

--- warmup2/main.js
+++ warmup2/main.js
@@ -2,9 +2,10 @@
 var drink = ['water', 'coffee']
 
 function flip(drink) {
-  var flipdrink = ['coffee', 'water']
-  for (var i = 0; i > drink.length; i--) {
-    flipdrink = drink
+  var reversed = []
+  var chug = drink.length
+  for (var i = (chug - 1); i >= 0; i--) {
+    reversed.push(drink[i])
   }
-  return flipdrink
+  return reversed
 }
