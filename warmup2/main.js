d3f85147b00142c32d5f9779220d96b2089395b6
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-07 13:03:43 -0800

    main.js for warmup2 added.

--- warmup2/main.js
+++ warmup2/main.js
@@ -1,11 +1,50 @@
 /* eslint-disable no-unused-vars */
-var drink = ['water', 'coffee']
+var $time = document.querySelector('#time')
+var $start = document.querySelector('#start')
+var $stop = document.querySelector('#stop')
+var $reset = document.querySelector('#reset')
+var $limit = document.querySelector('#limit')
+var $set = document.querySelector('#set')
 
-function flip(drink) {
-  var reversed = []
-  var chug = drink.length
-  for (var i = (chug - 1); i >= 0; i--) {
-    reversed.push(drink[i])
-  }
-  return reversed
+var timer = {
+  seconds: 0,
+  id: null,
+  limit: $limit.value
 }
+
+function start() {
+  if(timer.id === null){
+    timer.id = setInterval(function () {
+      timer.seconds++
+      render()
+      limit(event)
+    }, 1000)
+  }
+}
+
+function stop() {
+  clearInterval(timer.id)
+}
+
+function render() {
+  $time.textContent = timer.seconds
+}
+
+function reset() {
+  timer.seconds = 0
+  $time.textContent = timer.seconds
+}
+
+function limit(event) {
+  if(timer.seconds === $limit.value){
+    clearInterval(timer.id)
+  }
+}
+
+$start.addEventListener('click', start)
+
+$stop.addEventListener('click', stop)
+
+$reset.addEventListener('click', reset)
+
+$set.addEventListener('click', limit)
